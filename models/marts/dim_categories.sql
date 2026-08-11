93c8e0f5c4215e97ef942288efa777e92adb9c6d
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2026-08-11 07:16:44 -0400

    Add SQL model for categories and fix typos in companies model

--- models/marts/dim_categories.sql
+++ models/marts/dim_categories.sql
@@ -1,14 +1,14 @@
-with companies as (
+with categories as (
     select
-        company,
+        category,
         count(*) as posting_count,
-        count(distinct category) as category_count,
+        count(distinct company) as company_count,
         round(avg(salary_max), 0) as avg_salary_max,
         round(avg(salary_min), 0) as avg_salary_min
     from {{ ref('int_postings_deduplicated') }}
-    where company is not null
-    group by company
+    where category is not null
+    group by category
 )
 
-select * from companies
+select * from categories
 order by posting_count desc
